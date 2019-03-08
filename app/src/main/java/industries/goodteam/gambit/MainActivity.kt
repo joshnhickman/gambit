40fb31612ae2ba22d331d764d82912c2c865f1eb
joshnhickman <joshnhickman@gmail.com>
2019-03-08 02:17:13 +0000

    initial commit

---
+++ app/src/main/java/industries/goodteam/gambit/MainActivity.kt
@@ -0,0 +1,12 @@
+package industries.goodteam.gambit
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
