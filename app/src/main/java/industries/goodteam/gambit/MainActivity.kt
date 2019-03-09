d1b0b9323e2ee84edcbe5aa72c91b429a476f281
joshnhickman <joshnhickman@gmail.com>
2019-03-09 19:46:04 +0000

    add rotation through touch

--- app/src/main/java/industries/goodteam/gambit/MainActivity.kt
+++ app/src/main/java/industries/goodteam/gambit/MainActivity.kt
@@ -4,6 +4,9 @@ import android.app.Activity
 import android.content.Context
 import android.opengl.GLSurfaceView
 import android.os.Bundle
+import android.view.MotionEvent
+
+private const val TOUCH_SCALE_FACTOR = 180.0f / 320f
 
 class MainActivity : Activity() {
 
@@ -17,12 +20,45 @@ class MainActivity : Activity() {
     }
 
     class MyGLSurfaceView(context: Context) : GLSurfaceView(context) {
+
         private val renderer: MyGLRenderer
+
+        private var previousX: Float = 0f
+        private var previousY: Float = 0f
 
         init {
             setEGLContextClientVersion(3)
             renderer = MyGLRenderer()
             setRenderer(renderer)
+
+            renderMode = GLSurfaceView.RENDERMODE_WHEN_DIRTY
+        }
+
+        override fun onTouchEvent(e: MotionEvent): Boolean {
+            val x: Float = e.x
+            val y: Float = e.y
+
+            when (e.action) {
+                MotionEvent.ACTION_MOVE -> {
+                    var dx: Float = x - previousX
+                    var dy: Float = y - previousY
+
+                    if (y > height / 2) {
+                        dx *= -1
+                    }
+
+                    if (x < width / 2) {
+                        dy *= -1
+                    }
+
+                    renderer.angle += (dx + dy) * TOUCH_SCALE_FACTOR
+                    requestRender()
+                }
+            }
+
+            previousX = x
+            previousY = y
+            return true
         }
     }
 }
